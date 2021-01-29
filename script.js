3ec5d5c393b044cb6eae41e0d7f8475e0e2e0bd6
Christopher Watkins <chrisbwatkins@gmail.com>
2021-01-28 17:46:17 -0700

    first commit added some pseudo code to the file

---
+++ script.js
@@ -0,0 +1,22 @@
+var timeblocks,
+// moment Today, hours 
+
+// .ready(){
+
+
+    // start today's date
+
+    // build 9 time blocks
+
+        //blocks should have    
+
+        // a label thats the time 
+        // a textbox to store what we are doing 
+        // a submit button or save button to keep the text there 
+            // localstorage 
+        // maybe in n place holder hint hint????
+            // or text content be serious 
+    
+
+
+// }
